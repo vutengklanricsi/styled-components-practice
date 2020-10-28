b9962a826917f148bd735e71802843e2fbecc486
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-10-28 18:14:23 +0100

    button component declared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+import Button from './components/Button';
+
+const MainWrapper = styled.section`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 1200px;
+`;
 
 function App() {
   return (
-    <div className="App">
-    Hello
-    </div>
+    <MainWrapper className="App">
+      <Button>
+        Click on it!
+      </Button>
+    </MainWrapper>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Button = ({children}) => {
+	return (
+		<button>
+			{children}
+		</button>
+	)
+}
+
+export default Button
