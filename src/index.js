b31d06dafa5463c4affca83d3017a8a14dc970ca
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-11-02 15:51:18 +0100

    Global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import GlobalStyles from "./theme/globalStyles";
 
-render(<App />, document.getElementById("root"));
+render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
